6f1d3e9ebb6d3565c2058680639d4c00393fd091
sheelamswetha <sheelamswetha5@gmail.com>
2024-04-20 00:30:44 +0530

    first

---
+++ 01basicreact/src/index.js
@@ -0,0 +1,13 @@
+
+
+import App from './App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
---
+++ 01vitelatest/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+
+function App() {
+  
+
+  return (
+      <h1>welcome vite </h1>
+  )
+}
+
+export default App
